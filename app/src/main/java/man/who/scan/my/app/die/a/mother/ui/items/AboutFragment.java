94db07664e3f3a460aa45578228ae553c13ef73b
lijia <lijia0732@sina.com>
2021-09-09 11:48:07 +0800

    add About pages (source)

---
+++ app/src/main/java/man/who/scan/my/app/die/a/mother/ui/items/AboutFragment.java
@@ -0,0 +1,60 @@
+package man.who.scan.my.app.die.a.mother.ui.items;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+import man.who.scan.my.app.die.a.mother.R;
+import man.who.scan.my.app.die.a.mother.ui.FragmetActivity;
+import man.who.scan.my.app.die.a.mother.ui.base.BaseFragment;
+
+public class AboutFragment extends BaseFragment {
+
+    FragmetActivity activity;
+    View view;
+    WebView wb_about;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activity = (FragmetActivity) getActivity();
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.about, container, false);
+        TextView title = activity.findViewById(R.id.tv_title);
+        title.setText("关于");
+        wb_about = (WebView) view.findViewById(R.id.wb_about);
+        System.out.println(wb_about);
+        // 启用javascript
+        wb_about.getSettings().setJavaScriptEnabled(true);
+        // 从assets目录下面的加载html
+        wb_about.loadUrl("file:///android_asset/about/about.html");
+        wb_about.addJavascriptInterface(AboutFragment.this, "android");
+        return view;
+    }
+
+    //由于安全原因 需要加 @JavascriptInterface
+    @JavascriptInterface
+    public String getVersionName() {
+        Context context = this.getContext();
+        PackageManager manager = context.getPackageManager();
+        try {
+            PackageInfo info = manager.getPackageInfo(context.getPackageName(), 0);
+            return info.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return "Unknown";
+        }
+    }
+}
